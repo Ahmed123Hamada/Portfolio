5fc2831ebda35cad4a65c702ce253e2c2401f2d1
Ahmed123Hamada <wasdahmed033@gmail.com>
2025-03-10 15:39:53 +0200

    welcom an in fist deploy

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import About from "./components/About";
-import Nav1 from '../components/Navbarr ';
 import Main from "./components/Main";
 import SkillsSection from "./components/Skills";
 import Resume from "./components/Resume";
@@ -8,12 +7,13 @@ import Portofoilo from "./components/Portofoilo";
 import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
 import ContactSection from "./components/Contact";
+import Navbarr from "./components/Navbarr";
 export default function Home({ about }) {
   return (
     <>
       <div className="flex items-center justify-center min-h-screen w-full">
         <div className="w-[400px] max-lg:w-0 ">
-        <Nav1 />   
+          <Navbarr />   
         </div>
 <div className="flex flex-col items-center justify-center w-full gap-4 ">
         <Main  />
